22427d9ffbfaec603215dd3450d4243a371fb621
Бердыев Б.К <BerdyevBK@tomskasu.ru>
2022-07-13 16:19:30 +0700

    test: Комментарий классу и его членам

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -25,6 +25,13 @@ export class DataService {
     this.url = 'https://iap_dev2.tomskasu.ru/api/';
   }
 
+  /**
+   *
+   * @param column столбцы
+   * @param direction 1 | -1
+   * @param page страницы от 0
+   * @returns список ПБ
+   */
   public getListPassport(column: Column, direction: Direction, page: number) {
     const url = `${this.url}worker/list_passport`;
     const body = this.body(column, direction, page);
@@ -32,6 +39,11 @@ export class DataService {
     return this.postAPI(url, body);
   }
 
+  /**
+   *
+   * @param user логин пароль ремембер
+   * @returns запрос логин
+   */
   public login(user: User) {
     const url = `${this.url}login`;
     const body = {
@@ -55,10 +67,23 @@ export class DataService {
       });
   }
 
+  /**
+   *
+   * @param url url
+   * @param body тело запроса
+   * @returns пост метод
+   */
   private postAPI(url: string, body: any) {
     return this.http.post(url, body);
   }
 
+  /**
+   *
+   * @param columns столбцы
+   * @param directions для сортировки 1|-1
+   * @param page страница от 0
+   * @returns тело запроса
+   */
   private body = (columns: Column, directions: 1 | -1, page: number): RequestBody => ({
     id: '384c601d-875d-4797-b50b-ea796a9d4f36',
     jsonrpc: '2.0',
