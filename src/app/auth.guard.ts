788fec965d2a2639c3dc0af4109d3de71c08e684
Бердыев Б.К <BerdyevBK@tomskasu.ru>
2022-07-13 16:09:52 +0700

    test: Комментарий классу и его методу

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-
+/**
+ * Защита от неавторизованного пользователя
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
+  /**
+   *
+   * @returns проверка токена в localStorage
+   */
   public canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     if (localStorage.getItem('auth-token') !== null) {
       return true;
     }
     this.router.navigate(['/login']);
-    
-return false;
+
+    return false;
   }
 }
