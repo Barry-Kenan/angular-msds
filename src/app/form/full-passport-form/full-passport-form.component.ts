eb204d6098a063f99741aa0e5b73c90cd3d3bfc3
Бердыев Б.К <BerdyevBK@tomskasu.ru>
2022-07-13 10:45:16 +0700

    test: Удален неиспользуемый часть кода

--- src/app/form/full-passport-form/full-passport-form.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-full-passport-form',
-  templateUrl: './full-passport-form.component.html',
-  styleUrls: ['./full-passport-form.component.scss'],
-})
-export class FullPassportFormComponent {}
