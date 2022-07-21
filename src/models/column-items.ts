f734fbe3a91fb02665ce27f3e213acb5f4d6c130
Бердыев Б.К <BerdyevBK@tomskasu.ru>
2022-07-21 13:22:06 +0700

    tableFeature: комментарии

--- src/models/column-items.ts
+++ src/models/column-items.ts
@@ -4,8 +4,20 @@ import { ColumnName } from './column-name';
  * тип для списка столбцов
  */
 export interface ColumnItems {
+  /**
+   * Название для отображение колонок
+   */
   title: string;
+  /**
+   * Ширина столбцов
+   */
   width: string;
+  /**
+   * Название колонок
+   */
   columnName: ColumnName;
+  /**
+   * Функция для сортировки колонок
+   */
   sort?: Function;
 }
