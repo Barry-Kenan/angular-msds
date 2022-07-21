4942a545d82b640d80fbbbf7da6f914b5556f462
Бердыев Б.К <BerdyevBK@tomskasu.ru>
2022-07-21 16:50:25 +0700

    newPassportFormFeature: чтобы вернул объект с results

--- src/app/modules/shared/services/request.service/request.service.ts
+++ src/app/modules/shared/services/request.service/request.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from 'src/models/user';
 
@@ -39,6 +39,6 @@ export class RequestService {
       method,
     };
 
-    return this.http.post<T>(postUrl, body);
+    return this.http.post<T>(postUrl, body).pipe(map((res: any) => res.result));
   }
 }
